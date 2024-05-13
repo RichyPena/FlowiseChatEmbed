a4e0e9b57773c45d6376cbe4a5d4a275ddde7cb0
FarmingisLife.org <Info@farmingislife.com>
2024-05-13 08:19:29 -0400

    no branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> FarmingisLife</span>
+        <span> FarmingisLife!</span>
       </a>
     </span>
   );
